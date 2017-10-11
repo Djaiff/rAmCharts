d9e434cfa1fb139366de3286abe3a0440dfb93d2
Jeffery Petit <jeffery.petit@datastorm.fr>
2017-10-11 10:32:47 +0200

    Added external js library

--- R/utils_basicClassUnions.R
+++
@@ -1,5 +0,0 @@
-setClassUnion("characterOrMissing", c("character", "missing"))
-setClassUnion(name = "listOrCharacter", members = c("list", "character"))
-setClassUnion("logicalOrMissing", c("logical", "missing"))
-setClassUnion("numericOrCharacter", c("numeric", "character"))
-setClassUnion("characterOrNULL",members=c("character", "NULL"))
